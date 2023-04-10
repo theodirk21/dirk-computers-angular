494d9c4625ccbae5c2fcb228c48fe39d88d3ad56
Theo Dirk <theodirk17@gmail.com>
2023-04-10 14:40:56 -0300

    commit de post do sitr

---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduto, produtos } from '../produtos';
+import { ProdutosService } from '../produtos.service';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent implements OnInit{
+  
+  produtos: IProduto[] | undefined;
+  
+  constructor (
+    private produtosService: ProdutosService,
+    private router: ActivatedRoute
+    ){}
+  
+  
+    ngOnInit(): void {
+      const produto = this.produtosService.getAll();
+      this.router.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLowerCase();
+  
+        if (descricao) {
+          this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+          return;
+        }
+  
+        this.produtos = produtos;
+      });
+  }
+
+
+
+}
